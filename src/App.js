8cc1c2958c29871c915df75e3bc220107c50d5c9
Juhye O <artigen3@gmail.com>
2023-12-29 19:47:04 +0900

    create, edit, delete, update lists with axios

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from "axios";
 
 function App(){
+    //useState에 저장된 state는 새로고침 하면 초기화됨 -> 앱 시작 때 마다 API 호출 
     const [books, setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        
+        setBooks(response.data);
+    };
+
+    useEffect(()=> {
+        fetchBooks();
+    }, []); 
+    
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`,{
+            title: newTitle
+        });
+        
         const updatedBooks = books.map((book)=>{
             if(book.id === id) {
-                return {...book, title : newTitle};
+                return {...book, ...response.data};
             }
             return book;
         });
-
         setBooks(updatedBooks);
     };
     
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        axios.delete(`http://localhost:3001/books/${id}`);
+        
         const updatedBooks = books.filter((book)=> {
             return book.id !== id;
         });
         setBooks(updatedBooks);
     };
 
-    const createBooks = (title) => {
+    const createBooks = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
+  
         const updatedBooks = [
             ...books, 
-            {id : Math.round(Math.random() * 9999), title}
+            response.data
         ];
         setBooks(updatedBooks);
     };
